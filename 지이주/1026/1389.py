4e4ba9222bde562ebd972ccafe293932e36651a5
JiIJu <109330668+JiIJu@users.noreply.github.com>
2022-10-26 21:47:20 +0900

    Create 1389.py

---
+++ 지이주/1026/1389.py
@@ -0,0 +1,35 @@
+# 1389 케빈베이컨의 6단계 법칙
+import sys
+from collections import deque
+
+def bfs(data,s):
+    num = [0]*(N+1)
+    q = []
+    visit[s] = 1
+    q.append(s)
+    while q:
+        a = q.pop(0)
+        for i in data[a]:
+            if visit[i] == 0:
+                num[i] = num[a]+1
+                q.append(i)
+                visit[i] = 1
+    return sum(num)
+N,M = map(int,input().split())
+
+data = [[] for _ in range(N+1)]
+
+for i in range(M):
+    a,b = map(int,sys.stdin.readline().split())
+    data[a].append(b)
+    data[b].append(a)
+
+
+
+ans = []
+for i in range(1,N+1):
+    visit = [0] * (N + 1)
+    z = bfs(data,i)
+    ans.append(z)
+
+print(ans.index(min(ans))+1)
