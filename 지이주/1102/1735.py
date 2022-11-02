34ca42a24a75926ab5e5248611ddb392e489853b
JiIJu <109330668+JiIJu@users.noreply.github.com>
2022-11-02 17:59:50 +0900

    1102

---
+++ 지이주/1102/1735.py
@@ -0,0 +1,18 @@
+a,b = map(int,input().split())
+c,d = map(int,input().split())
+
+e = a*d+b*c
+f = b*d
+
+A , B = max(e,f) , min(e,f)
+
+# 유클리드 호제법
+while A%B!=0:
+    ans = A%B
+    A = B
+    B = ans
+
+if B==1:
+    print(f'{e} {f}')
+else:
+    print(f'{e//B} {f//B}')
